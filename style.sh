df0addbb1ddf3c0a65baa4bd94e3e5eb82558e5c
Alexander Kernozhitsky <sh200105@mail.ru>
2019-02-24 01:40:48 +0300

    Fix dashes

--- style.sh
+++ style.sh
@@ -20,34 +20,50 @@ error() {
 
 check() {
 	GREP_PARAMS=(--color -C 1 -nEH)
+	while :; do
+		case "$1" in
+			--)
+				shift
+				break
+			;;
+			*)
+				break
+			;;
+		esac
+	done
 	grep "${GREP_PARAMS[@]}" "$2" "$1" && error "$3"
 }
 
 check_file() {
-	check "$1" \
+	check -- "$1" \
 	      '^.{80,}$' \
 	      'Some lines exceed 80 characters in length'
 	 
-	check "$1" \
+	check -- "$1" \
 	      '\$\$' \
 	      '$$ found, better use \[ \]'
 	
-	check "$1" \
+	check -- "$1" \
 	      '\\Rightarrow'\
 	      'Use \implies'
 	
-	check "$1" \
+	check -- "$1" \
 	      '\\Leftrightarrow' \
 	      'Use \iff'
 	
-	check "$1" \
+	check -- "$1" \
 	      '\s(\\left)?\([0-9]+\.?[0-9]*(\\right)?\)' \
 	      'Seems that you are trying to reference a formula directly, like (1.5).
 Use equation and \eqref{}.'
 	
-	check "$1" \
+	check -- "$1" \
 	      '\\textit' \
 	      'Better use \emph instead of \textit'
+	
+	check -- "$1" \
+	      '( - *[а-яА-Я])|([а-яА-Я] *- )|([а-яА-Я](\$[^\$]{1,20}\$| |\})*- *$)' \
+	      'Use ~--- for a dash. Example:
+Это~--- тире'
 }
 
 export -f error
